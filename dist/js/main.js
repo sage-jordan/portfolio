f9b179119e91df779c48800d56550ee5d392fe7d
sage-jordan <sagemjordan@gmail.com>
2020-01-25 23:41:22 -0500

    Menu button was showing;added JS to toggle classes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,33 +1,35 @@
 // Select DOM Items
+
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-items');
 
-// Set Initial State Of Menu
-let showMenu = false;
+// Set Initial State of Menu
 
-menuBtn.addEventListener('click', toggleMenu);
+let showMenu = false; // State T/F
 
-function toggleMenu() {
-  if (!showMenu) {
-    menuBtn.classList.add('close');
-    menu.classList.add('show');
-    menuNav.classList.add('show');
-    menuBranding.classList.add('show');
-    navItems.forEach(item => item.classList.add('show'));
+menuBtn.addEventListener('click', toggleMenu); // Event Listner
 
-    // Set Menu State
-    showMenu = true;
-  } else {
-    menuBtn.classList.remove('close');
-    menu.classList.remove('show');
-    menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
-    navItems.forEach(item => item.classList.remove('show'));
-
-    // Set Menu State
-    showMenu = false;
-  }
-}
+function toggleMenu() { // Toggle Menu (classes and state)
+    if(!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuBranding.classList.add('show');
+        menuNav.classList.add('show');
+        navItems.forEach(item => {
+            item.classLlist.add('show');
+        })
+        showMenu = true; 
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuBranding.classList.remove('show');
+        menuNav.classList.remove('show');
+        navItems.forEach(item => {
+            item.classLlist.remove('show');
+        })
+        showMenu = false;
+    }
+};
